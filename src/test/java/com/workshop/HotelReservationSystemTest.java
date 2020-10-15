a7cbcc1baf4e94340ef746aaf59269d66889849d
ibalram <balramsinghrathore3@gmail.com>
2020-10-16 04:48:14 +0530

    Added: Ability to find cheapest bestrated hotel for reward customer using streams

--- src/test/java/com/workshop/HotelReservationSystemTest.java
+++ src/test/java/com/workshop/HotelReservationSystemTest.java
@@ -9,25 +9,36 @@ import static org.junit.Assert.*;
 public class HotelReservationSystemTest {
 	@Test
 	public void testGetCheapestHotelForDateRange() {
-		HotelReservationSystem obj  = new HotelReservationSystem();
-		assertEquals("Lakewood, Rating: 3 and Total Rates: $220", obj.getCheapestHotel("10Sep2020", "11Sep2020", "Regular").toString());
+		HotelReservationSystem obj = new HotelReservationSystem();
+		assertEquals("Lakewood, Rating: 3 and Total Rates: $220",
+				obj.getCheapestHotel("10Sep2020", "11Sep2020", "Regular").toString());
 	}
-	
+
 	@Test
 	public void testGetCheapestHotelForDateRangeWithWeekends() {
-		HotelReservationSystem obj  = new HotelReservationSystem();
-		assertEquals("Lakewood, Rating: 3 and Total Rates: $200", obj.getCheapestHotel("11Sep2020", "12Sep2020", "Regular").toString());
+		HotelReservationSystem obj = new HotelReservationSystem();
+		assertEquals("Lakewood, Rating: 3 and Total Rates: $200",
+				obj.getCheapestHotel("11Sep2020", "12Sep2020", "Regular").toString());
 	}
-	
+
 	@Test
 	public void testGetCheapestHotelForDateRangeWithWeekendsWithBestRating() {
-		HotelReservationSystem obj  = new HotelReservationSystem();
-		assertEquals("Bridgewood, Rating: 4 and Total Rates: $200", obj.getCheapestBestRatedHotel("11Sep2020", "12Sep2020", "Regular").toString());
+		HotelReservationSystem obj = new HotelReservationSystem();
+		assertEquals("Bridgewood, Rating: 4 and Total Rates: $200",
+				obj.getCheapestBestRatedHotel("11Sep2020", "12Sep2020", "Regular").toString());
 	}
-	
+
 	@Test
 	public void testGetBestRatedHotelForDateRangeWithWeekends() {
-		HotelReservationSystem obj  = new HotelReservationSystem();
-		assertEquals("Ridgewood, Rating: 5 and Total Rates: $370", obj.getBestRatedHotel("11Sep2020", "12Sep2020", "Regular").toString());
+		HotelReservationSystem obj = new HotelReservationSystem();
+		assertEquals("Ridgewood, Rating: 5 and Total Rates: $370",
+				obj.getBestRatedHotel("11Sep2020", "12Sep2020", "Regular").toString());
+	}
+
+	@Test
+	public void testGetCheapestHotelForDateRangeWithWeekendsWithBestRatingRewardCustomer() {
+		HotelReservationSystem obj = new HotelReservationSystem();
+		assertEquals("Ridgewood, Rating: 5 and Total Rates: $140",
+				obj.getCheapestBestRatedHotel("11Sep2020", "12Sep2020", "Reward").toString());
 	}
 }
